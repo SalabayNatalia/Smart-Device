38a5946df1b5496a405283e08c743e89b78c0a53
unknown <salabay.natalia89@gmail.com>
2022-09-23 00:24:54 +0300

    Исправления после тестирования

--- source/js/modules/nojs.js
+++ source/js/modules/nojs.js
@@ -1,5 +1,19 @@
+// export const nojs = () => {
+//   if (document.querySelector('.nojs')) {
+//     document.querySelector('.nojs').classList.remove('nojs');
+//   }
+// };
 export const nojs = () => {
   if (document.querySelector('.nojs')) {
     document.querySelector('.nojs').classList.remove('nojs');
+    const phones = document.querySelectorAll('[data-phone]');
+    phones.forEach(function (item) {
+      item.removeAttribute('pattern');
+    });
   }
 };
+//  function closeAccordion() {
+//     accordion.forEach(function (item) {
+//       item.classList.remove('is-open');
+//     });
+//   }
