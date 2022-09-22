9aa4a53aff8b912f6a251f9807152d9ae6cdce6d
unknown <salabay.natalia89@gmail.com>
2022-09-22 03:15:25 +0300

    Правки

--- source/js/main.js
+++ source/js/main.js
@@ -1,21 +1,28 @@
 import {iosVhFix} from './utils/ios-vh-fix';
-import {createModal} from './modules/modal';
+import {initModals} from './modules/modals/init-modals';
 import {anchor} from './modules/anchor';
 import {openText} from './modules/collapsible';
 import {openAccordion} from './modules/accordion';
 import {getMask} from './modules/mask';
-
-const body = document.querySelector('body');
-
-body.classList.remove('nojs');
+// ---------------------------------
 
 window.addEventListener('DOMContentLoaded', () => {
+
+  // Utils
+  // ---------------------------------
+
   iosVhFix();
+
+  // Modules
+  // ---------------------------------
+
+  // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
+  // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
-    createModal();
     anchor();
     openText();
     openAccordion();
     getMask();
+    initModals();
   });
 });
--- source/js/modules/mask.js
+++ source/js/modules/mask.js
@@ -1,37 +1,3 @@
-// const getMask = () => {
-//   var eventCalllback = function (e) {
-//     var el = e.target,
-//       clearVal = el.dataset.phoneClear,
-//       pattern = el.dataset.phonePattern,
-//       matrixDef = '+7(___) ___-__-__',
-//       matrix = pattern ? pattern : matrixDef,
-//       i = 0,
-//       def = matrix.replace(/\D/g, '');
-//     val = e.target.value.replace(/\D/g, '');
-//     if (clearVal !== 'false' && e.type === 'blur') {
-//       if (val.length < matrix.match(/([\_\d])/g).length) {
-//         e.target.value = '';
-//                 console.log('ddd');
-
-//         return;
-//       }
-//     }
-//     if (def.length >= val.length)
-//       val = def;
-//     e.target.value = matrix.replace(/./g, function (a) {
-//       return /[_\d]/.test(a) && i < val.length ? val.charAt(i++) : i >= val.length ? '' : a;
-//     });
-//   };
-//   const phoneInputs = document.querySelectorAll('[data-phone-pattern]');
-//   for (let elem of phoneInputs) {
-//     for (let ev of ['input', 'blur', 'focus']) {
-//       elem.addEventListener(ev, eventCalllback);
-//     }
-//   }
-// };
-
-// export {getMask};
-
 const getMask = () => {
   var eventCalllback = function (e) {
         var el = e.target,
